09db9309396865201e4534ced41755e70914a4bd
zach beecher <zachbeecher@gmail.com>
2018-05-14 08:51:08 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'DRw7obY8EDS1RXg6q0CnD2yOV',  
+    consumer_secret: '1Fb5wzI1kH9E5geisANfp0iZpUJmTe9xQlsRhkAxw7ilJ5v0Y9',
+    access_token: '181855639-qfVPDQL0QIsjGW2754Uu59gS6SLEM7tvPr2WLNiz',  
+    access_token_secret: 'WL31V6LzOgVaasRyOMLv9gjAi2peqLzSdpIXjNjtV5edo'
+  }
